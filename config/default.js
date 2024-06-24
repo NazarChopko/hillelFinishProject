d1bc40a4cd9c79ec866562c40e3102221640458d
Nazar Chopko <nazarchopko@MacBook-Pro-Nazar.local>
2024-06-24 11:07:03 +0300

    set up secure false

--- config/default.js
+++ config/default.js
@@ -1,9 +1,8 @@
-const isProd = process.env.MODE === 'production';
 const expiredTime = 1000 * 60 * 60 * 24;
 
 module.exports = {
   port: process.env.PORT || 3009,
-  cookieOptions: { httpOnly: true, sameSite: 'strict', secure: isProd, expires: expiredTime },
+  cookieOptions: { httpOnly: true, sameSite: 'strict', secure: false, expires: expiredTime },
   sessionName: process.env.SESSION_NAME || 'sessionId',
   secret: process.env.SECRET || 'secret secret',
   expiredStorage: expiredTime
