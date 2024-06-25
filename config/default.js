7fe7ccdcb4d4bbc83cd2ac7088ddfc0ef0fa447e
Nazar Chopko <nazarchopko@MacBook-Pro-Nazar.local>
2024-06-25 14:05:32 +0300

    added docker

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 const expiredTime = 1000 * 60 * 60 * 24;
 
 module.exports = {
-  port: process.env.PORT || 3009,
+  port: 3000,
   cookieOptions: { httpOnly: true, sameSite: 'strict', secure: false, expires: expiredTime },
   sessionName: process.env.SESSION_NAME || 'sessionId',
   secret: process.env.SECRET || 'secret secret',
